ec27a6e97939a7de8eb86bab188a4737aad9bc78
Renz <renzbarte038@gmail.com>
2025-02-07 11:02:51 +0800

    init commit

---
+++ src/main/java/com/renz/valentine/ValentineApplication.java
@@ -0,0 +1,13 @@
+package com.renz.valentine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValentineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValentineApplication.class, args);
+	}
+
+}
